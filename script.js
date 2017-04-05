5e4edc430f7c39d4927d8a8b5daa9351ef8831cf
Adam Hoff <swimmerdude79@comcast.net>
2017-04-05 15:11:50 -0400

    Final submission no comments

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ var currentMoves = 0;
 function _(id) {
   return document.getElementById(id);
 }
+_("reset").onclick = function() {
+  currentMoves = 0;
+  _("moves").innerHTML = "Moves: " + currentMoves;
+  onchange_handler();
+  while (_("middleRod").hasChildNodes()) {
+    _("middleRod").removeChild(_("middleRod").lastChild);
+  }
+  while (_("endRod").hasChildNodes()) {
+    _("endRod").removeChild(_("endRod").lastChild);
+  }
+};
 
 function dragstart_handler(event) {
   event.dataTransfer.dropEffect = "move";
@@ -30,75 +41,119 @@ function dragover_handler(event) {
 }
 
 function onchange_handler(select) {
-  switch (_("diskAmount").value) {
+  switch (_("discAmount").value) {
     case '3':
-    _("disk4").style.display = 'none';
-    _("disk5").style.display = 'none';
-    _("disk6").style.display = 'none';
-    _("disk7").style.display = 'none';
-    _("disk8").style.display = 'none';
+    _("startingRod").append(_("disc1"));
+    _("startingRod").append(_("disc2"));
+    _("startingRod").append(_("disc3"));
+    _("disc1").style.display = 'flex';
+    _("disc2").style.display = 'flex';
+    _("disc3").style.display = 'flex';
+    _("disc4").style.display = 'none';
+    _("disc5").style.display = 'none';
+    _("disc6").style.display = 'none';
+    _("disc7").style.display = 'none';
+    _("disc8").style.display = 'none';
     _("minMoves").innerHTML = "Minimum Moves: 7"
     break;
     case '4':
-    _("startingRod").append(_("disk4"));
-    _("disk4").style.display = 'flex';
-    _("disk5").style.display = 'none';
-    _("disk6").style.display = 'none';
-    _("disk7").style.display = 'none';
-    _("disk8").style.display = 'none';
+    _("startingRod").append(_("disc1"));
+    _("startingRod").append(_("disc2"));
+    _("startingRod").append(_("disc3"));
+    _("startingRod").append(_("disc4"));
+    _("disc1").style.display = 'flex';
+    _("disc2").style.display = 'flex';
+    _("disc3").style.display = 'flex';
+    _("disc4").style.display = 'flex';
+    _("disc5").style.display = 'none';
+    _("disc6").style.display = 'none';
+    _("disc7").style.display = 'none';
+    _("disc8").style.display = 'none';
     _("minMoves").innerHTML = "Minimum Moves: 15"
     break;
     case '5':
-    _("startingRod").append(_("disk4"));
-    _("startingRod").append(_("disk5"));
-    _("disk4").style.display = 'flex';
-    _("disk5").style.display = 'flex';
-    _("disk6").style.display = 'none';
-    _("disk7").style.display = 'none';
-    _("disk8").style.display = 'none';
+    _("startingRod").append(_("disc1"));
+    _("startingRod").append(_("disc2"));
+    _("startingRod").append(_("disc3"));
+    _("startingRod").append(_("disc4"));
+    _("startingRod").append(_("disc5"));
+    _("disc1").style.display = 'flex';
+    _("disc2").style.display = 'flex';
+    _("disc3").style.display = 'flex';
+    _("disc4").style.display = 'flex';
+    _("disc5").style.display = 'flex';
+    _("disc6").style.display = 'none';
+    _("disc7").style.display = 'none';
+    _("disc8").style.display = 'none';
     _("minMoves").innerHTML = "Minimum Moves: 31"
     break;
     case '6':
-    _("startingRod").append(_("disk4"));
-    _("startingRod").append(_("disk5"));
-    _("startingRod").append(_("disk6"));
-    _("disk4").style.display = 'flex';
-    _("disk5").style.display = 'flex';
-    _("disk6").style.display = 'flex';
-    _("disk7").style.display = 'none';
-    _("disk8").style.display = 'none';
+    _("startingRod").append(_("disc1"));
+    _("startingRod").append(_("disc2"));
+    _("startingRod").append(_("disc3"));
+    _("startingRod").append(_("disc4"));
+    _("startingRod").append(_("disc5"));
+    _("startingRod").append(_("disc6"));
+    _("disc1").style.display = 'flex';
+    _("disc2").style.display = 'flex';
+    _("disc3").style.display = 'flex';
+    _("disc4").style.display = 'flex';
+    _("disc5").style.display = 'flex';
+    _("disc6").style.display = 'flex';
+    _("disc7").style.display = 'none';
+    _("disc8").style.display = 'none';
     _("minMoves").innerHTML = "Minimum Moves: 63"
     break;
     case '7':
-    _("startingRod").append(_("disk4"));
-    _("startingRod").append(_("disk5"));
-    _("startingRod").append(_("disk6"));
-    _("startingRod").append(_("disk7"));
-    _("disk4").style.display = 'flex';
-    _("disk5").style.display = 'flex';
-    _("disk6").style.display = 'flex';
-    _("disk7").style.display = 'flex';
-    _("disk8").style.display = 'none';
+    _("startingRod").append(_("disc1"));
+    _("startingRod").append(_("disc2"));
+    _("startingRod").append(_("disc3"));
+    _("startingRod").append(_("disc4"));
+    _("startingRod").append(_("disc5"));
+    _("startingRod").append(_("disc6"));
+    _("startingRod").append(_("disc7"));
+    _("disc1").style.display = 'flex';
+    _("disc2").style.display = 'flex';
+    _("disc3").style.display = 'flex';
+    _("disc4").style.display = 'flex';
+    _("disc5").style.display = 'flex';
+    _("disc6").style.display = 'flex';
+    _("disc7").style.display = 'flex';
+    _("disc8").style.display = 'none';
     _("minMoves").innerHTML = "Minimum Moves: 127"
     break;
     case '8':
-    _("startingRod").append(_("disk4"));
-    _("startingRod").append(_("disk5"));
-    _("startingRod").append(_("disk6"));
-    _("startingRod").append(_("disk7"));
-    _("startingRod").append(_("disk8"));
-    _("disk4").style.display = 'flex';
-    _("disk5").style.display = 'flex';
-    _("disk6").style.display = 'flex';
-    _("disk7").style.display = 'flex';
-    _("disk8").style.display = 'flex';
+    _("startingRod").append(_("disc1"));
+    _("startingRod").append(_("disc2"));
+    _("startingRod").append(_("disc3"));
+    _("startingRod").append(_("disc4"));
+    _("startingRod").append(_("disc5"));
+    _("startingRod").append(_("disc6"));
+    _("startingRod").append(_("disc7"));
+    _("startingRod").append(_("disc8"));
+    _("disc1").style.display = 'flex';
+    _("disc2").style.display = 'flex';
+    _("disc3").style.display = 'flex';
+    _("disc4").style.display = 'flex';
+    _("disc5").style.display = 'flex';
+    _("disc6").style.display = 'flex';
+    _("disc7").style.display = 'flex';
+    _("disc8").style.display = 'flex';
     _("minMoves").innerHTML = "Minimum Moves: 255"
     break;
     default:
-    _("disk4").style.display = 'none';
-    _("disk5").style.display = 'none';
-    _("disk6").style.display = 'none';
-    _("disk7").style.display = 'none';
-    _("disk8").style.display = 'none';
+    _("startingRod").append(_("disc1"));
+    _("startingRod").append(_("disc2"));
+    _("startingRod").append(_("disc3"));
+    _("disc1").style.display = 'flex';
+    _("disc2").style.display = 'flex';
+    _("disc3").style.display = 'flex';
+    _("disc4").style.display = 'none';
+    _("disc5").style.display = 'none';
+    _("disc6").style.display = 'none';
+    _("disc7").style.display = 'none';
+    _("disc8").style.display = 'none';
+    _("minMoves").innerHTML = "Minimum Moves: 7"
   }
 }
+onchange_handler();
